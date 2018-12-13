1682a3cae83e17a04c0e536b94bd598e276c02b0
smanimozhiyan <manimozhiyans@gmail.com>
2018-12-13 11:49:26 +0530

    remove read from FS

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,17 +14,17 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
+        ClassLoader cl = CsvUtils.class.getClassLoader();
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+
+
+        Scanner scanner = new Scanner(cl.getResourceAsStream(path)).useDelimiter("\\A");
+        //System.out.println(ClassLoader.getSystemClassLoader().);
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
     }
 
